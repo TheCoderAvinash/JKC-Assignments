156ed420d56d9d893694e8054fd8089da865d312
Avinash Kumar Mallick <avinashkm152@gmail.com>
2023-09-01 16:12:39 +0530

    Assignment23-08-2023

---
+++ lengthConverter.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main(){
+    float dis,meters,centis;
+    printf("Enter the distance");
+    scanf("%f", &dis); //input for distance
+
+    meters= dis*1000;//converts to meters
+    centis= meters*100;
+    printf("Distance in km= %f", dis);
+    printf("Distance in m= %f", meters);
+    printf("Distance in cm= %f", centis);
+
+}
---
+++ temperatureConverter.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    float c,f;
+    int t;
+    printf("Type 1 for Celcius to Farenheit\nType 2 for Farenheit to Celcius\nType:");
+    scanf("%d",&t);
+    switch(t){
+        case 1:
+        printf("Enter the value in Celcius:");
+        scanf("%f", &c);
+        f=((9*c)/5)+32;
+        printf("The temperature in Farenheit is %.2fF", f);
+        break;
+        case 2:
+         printf("Enter the value in Farenheit:");
+        scanf("%f", &f);
+        c=(5*(f-32))/9;
+        printf("The temperature in Celcius is %.2fC", c);
+        break;
+
+    }
+return 0;
+}
