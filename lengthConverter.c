e764d1ed4d36962ed501bdfd257cccef1f5475b4
Avinash Kumar Mallick <avinashkm152@gmail.com>
2023-09-03 02:40:39 +0530

    updation

--- lengthConverter.c
+++ lengthConverter.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 int main(){
     float dis,meters,centis;
-    printf("Enter the distance");
+    printf("Enter the distance: ");
     scanf("%f", &dis); //input for distance
 
     meters= dis*1000;//converts to meters
     centis= meters*100;
-    printf("Distance in km= %f", dis);
-    printf("Distance in m= %f", meters);
-    printf("Distance in cm= %f", centis);
+    printf("Distance in km= %.2f\n", dis);
+    printf("Distance in m= %.2f\n", meters);
+    printf("Distance in cm= %.2f", centis);
 return 0;
 }
