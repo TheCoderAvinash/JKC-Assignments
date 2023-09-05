a49f0bd096a99d6070e33b1f4712209689c5add6
Avinash Kumar Mallick <avinashkm152@gmail.com>
2023-09-06 01:51:18 +0530

    finalCommit05052023

--- clothStore.c
+++ clothStore.c
@@ -1,26 +1,37 @@
-/*s */
+/*A cloth showroom has announced festival discounts and the gifts on the purchase of
+ items, based on the total cost as given below:
+ Total Cost	          Discount	         Gift
+ Up to ₹ 2,000	           5%	        Calculator
+ ₹ 2,001 to ₹ 5,000   	  10%	        School Bag
+ ₹ 5,001 to ₹ 10,000   	  15%	        Wall Clock
+ Above ₹ 10,000	          20%	        Wrist Watch
+ Write a program to input the total cost. Compute and display the amount to be paid
+ by the customer along with the gift. */
 
 #include <stdio.h>
 #include <string.h>
 int main(){
     int cost;
-    float discount;
+
     printf("Enter the Total Cost: ");
     scanf("%d", &cost);
     if(cost<=2000){
-        discount = 5;
+      cost = cost- (cost/20);//5% discount
+      printf("Gift: Calculator\n");
     }
     else if(cost<=5000){
-        discount = 10;
+      cost = cost- (cost/10);//10% discount
+       printf("Gift: School Bag\n");
     }
 
     else if(cost<=10000){
-        discount = 15;
+      cost = cost-((15*cost)/100);//15% discount
+       printf("Gift: Wall Clock\n");
     }
     else{
-        discount = 20;
+      cost = cost- (cost/5);//20% discount
+       printf("Gift: Wrist Watch\n");
     }
-
-    printf("---Your Bill---\nTotal Cost: %d \nDiscount: %.1f%%\nGift:", cost, discount);
+    printf("Total Cost: %d\n", cost);
     return 0;
 }
