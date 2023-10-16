658949c0ece2316359fddd21f12e2e8e69f15ff6
Avinash Kumar Mallick <avinashkm152@gmail.com>
2023-10-17 01:47:54 +0530

    AssignmentAndUpdates

---
+++ Switch-Case/4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+    float s=0;
+    int n;
+    char o;
+    printf("Enter (a) for series {0,3,8,15,24...n}\nEnter (b) to find the sum of 1/2 + 3/4 +...+19/20\nYour Choice: ");
+    scanf("%c",&o);
+    switch (o)
+    {
+    case 97:
+        printf("Enter the value of n: ");
+        scanf("%d", &n);
+        for(int i=3;s<=n;i+=2){
+            printf("%.f ", s);
+            s+=i;
+        }
+        break;
+    case 98:
+        for(int i=1;i<20;i++){
+            printf("%d/%d",i,i+1);
+            s+= (float)i/(i+1);
+            if(i!=19){
+                printf(" + ");
+            }
+        }
+        printf(" = %.2f", s);
+        break;
+    default:
+        printf("Enter the correct option");
+        break;
+    }
+
+    return 0;
+}
