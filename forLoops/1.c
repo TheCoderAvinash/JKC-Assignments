7ba838ce2a1ebfdd822428ae9068f85d9506b5ae
Avinash Kumar Mallick <avinashkm152@gmail.com>
2023-12-06 01:22:05 +0530

    LOOPS

--- forLoops/1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int i, n;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-    for(i=1; i<=10;i++)
-    printf("%d x %d = %d\n", n,i,n*i);
-    return 0;
-}
